1eb6ed7f6311f670c7ad66ae4686902c77d0236b
Firas Naber <firasnaber@gmail.com>
2022-09-11 22:03:24 -0500

    Add initial snake segments

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.title("Snake")
 screen.bgcolor("black")
+
+x_coord = 0
+for _ in range(3):
+    segment = Turtle(shape="square")
+    segment.color("white")
+    segment.goto(x=x_coord, y=0)
+    x_coord -= 20
 
 screen.exitonclick()
