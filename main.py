ac09117108022648a471ce4af8dcd388243a6539
Firas Naber <firasnaber@gmail.com>
2022-09-11 22:35:19 -0500

    Rewrite coding using a class

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.title("Snake")
 screen.bgcolor("black")
-
 screen.tracer(0)
 
-x_coord = 0
-segments = []
-for _ in range(3):
-    segment = Turtle(shape="square")
-    segment.color("white")
-    segment.penup()
-    segment.goto(x=x_coord, y=0)
-    segments.append(segment)
-    x_coord -= 20
+snake = Snake()
 
 game_over = False
 while not game_over:
     screen.update()
     time.sleep(0.1)
-    for seg_num in range(len(segments) - 1, 0, -1):
-        segments[seg_num].goto(segments[seg_num - 1].pos())
-    segments[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+SEGMENT_SIZE = 20
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        x_coord = 0
+        for _ in range(3):
+            segment = Turtle(shape="square")
+            segment.color("white")
+            segment.penup()
+            segment.goto(x=x_coord, y=0)
+            self.segments.append(segment)
+            x_coord -= 20
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            self.segments[seg_num].goto(self.segments[seg_num - 1].pos())
+        self.segments[0].forward(SEGMENT_SIZE)
