b5db2ab09970768de8781224d4b2a504d918cfd6
Firas <firasnaber@gmail.com>
2022-09-28 23:12:21 -0500

    Update snake class

--- snake.py
+++ snake.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
-
-SEGMENT_SIZE = 20
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
 LEFT = 180
@@ -15,26 +15,32 @@ class Snake:
         self.head = self.segments[0]
 
     def create_snake(self):
-        x_coord = 0
-        for _ in range(3):
-            segment = Turtle(shape="square")
-            segment.color("white")
-            segment.penup()
-            segment.goto(x=x_coord, y=0)
-            self.segments.append(segment)
-            x_coord -= 20
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def reset(self):
+        for i in self.segments:
+            i.hideturtle()
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def extend(self):
-        segment = Turtle(shape="square")
-        segment.color("white")
-        segment.penup()
-        segment.goto(x=self.segments[-1].xcor() - 20, y=0)
-        self.segments.append(segment)
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
-            self.segments[seg_num].goto(self.segments[seg_num - 1].pos())
-        self.segments[0].forward(SEGMENT_SIZE)
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
         if self.head.heading() != DOWN:
